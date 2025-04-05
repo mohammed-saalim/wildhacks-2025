a065745598387ee7a5f612314cb34559584f7659
derickjohnson242 <djohn242@depaul.edu>
2025-04-05 15:36:17 -0500

    Update App.js

--- interview-ai-frontend/src/App.js
+++ interview-ai-frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import InterviewPage from "./pages/InterviewPage";
+import ResumeUploader from "./pages/ResumeUploader";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/upload" element={<ResumeUploader />} />
       </Routes>
     </Router>
   );
