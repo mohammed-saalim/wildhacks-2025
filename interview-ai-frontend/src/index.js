6a4cad8ebb7f32377f3ae2cdf5aaf9fe2703aa86
Eric <180332937+StubbyGuy@users.noreply.github.com>
2025-04-05 17:30:51 -0500

    "Updated index.js and with Okta setup and Landing.js with Okta setup"

--- interview-ai-frontend/src/index.js
+++ interview-ai-frontend/src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'; 
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Security } from '@okta/okta-react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Configure Okta with your custom info
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-04106745.okta.com/oauth2/default',
+  clientId: '0oao5tp74nyYxFt0i5d7',
+  redirectUri: 'http://localhost:3000/login/callback',
+  pkce: true
+});
+
+// Render with createRoot
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Security oktaAuth={oktaAuth}>
     <App />
-  </React.StrictMode>
+  </Security>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+
+
+
+
+
