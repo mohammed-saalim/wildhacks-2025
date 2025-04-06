fb014127942cdbceba6bd66b6f2180f452875571
derickjohnson242 <djohn242@depaul.edu>
2025-04-06 03:04:01 -0500

    Update App.js

--- interview-ai-frontend/src/App.js
+++ interview-ai-frontend/src/App.js
@@ -1,67 +1,32 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import SecureRoute from "./pages/SecureRoute";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 import InterviewPage from "./pages/InterviewPage";
 import ResumeUploader from "./pages/ResumeUploader";
+import ProfilePage from "./pages/ProfilePage";
+import InterviewSetupPage from "./pages/InterviewSetupPage";
 import Navbar from "./components/Navbar";
 import FeedbackPage from "./pages/FeedbackPage";
-import { LoginCallback } from '@okta/okta-react';
-import AuthPage from "./pages/AuthPage.js";
-import Landing from "./pages/Landing.js"; 
+import Footer from "./components/Footer";
+
 
 
 function App() {
+
+
   return (
     <Router>
       <Navbar />
       <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<AuthPage />} /> {/* Login page */}
-        <Route path="/login/callback" element={<LoginCallback />} /> {/* Okta callback */}
-
-        {/* Protected Routes */}
-        <Route 
-          path="/landing" 
-          element={
-            <SecureRoute>
-              <Landing />
-            </SecureRoute>
-          } 
-        />
-        <Route 
-          path="/home" 
-          element={
-            <SecureRoute>
-              <HomePage />
-            </SecureRoute>
-          } 
-        />
-        <Route 
-          path="/interview" 
-          element={
-            <SecureRoute>
-              <InterviewPage />
-            </SecureRoute>
-          } 
-        />
-        <Route 
-          path="/upload" 
-          element={
-            <SecureRoute>
-              <ResumeUploader />
-            </SecureRoute>
-          } 
-        />
-        <Route 
-          path="/feedback" 
-          element={
-            <SecureRoute>
-              <FeedbackPage />
-            </SecureRoute>
-          } 
-        />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/authenticate" element={<AuthPage />} />
+        <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/interview-setup" element={<InterviewSetupPage />} />
+        <Route path="/upload" element={<ResumeUploader />} />
+        <Route path="/feedback" element={<FeedbackPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
