f9ad1bce9e752c02392e8bd76303798bf5239fab
Eric <180332937+StubbyGuy@users.noreply.github.com>
2025-04-06 01:50:45 -0500

    Update index.js with Okta configuration and routing

--- interview-ai-frontend/src/App.js
+++ interview-ai-frontend/src/App.js
@@ -1,25 +1,65 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SecureRoute from "./pages/SecureRoute";
 import HomePage from "./pages/HomePage";
-import AuthPage from "./pages/AuthPage";
 import InterviewPage from "./pages/InterviewPage";
 import ResumeUploader from "./pages/ResumeUploader";
 import Navbar from "./components/Navbar";
 import FeedbackPage from "./pages/FeedbackPage";
-
-
+import { LoginCallback } from '@okta/okta-react';
+import AuthPage from "./pages/AuthPage.js";
+import Landing from "./pages/Landing.js"; 
 
 function App() {
-
-
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/authenticate" element={<AuthPage />} />
-        <Route path="/interview" element={<InterviewPage />} />
-        <Route path="/upload" element={<ResumeUploader />} />
-        <Route path="/feedback" element={<FeedbackPage />} />
+        {/* Public Routes */}
+        <Route path="/" element={<AuthPage />} /> {/* Login page */}
+        <Route path="/login/callback" element={<LoginCallback />} /> {/* Okta callback */}
+
+        {/* Protected Routes */}
+        <Route 
+          path="/landing" 
+          element={
+            <SecureRoute>
+              <Landing />
+            </SecureRoute>
+          } 
+        />
+        <Route 
+          path="/home" 
+          element={
+            <SecureRoute>
+              <HomePage />
+            </SecureRoute>
+          } 
+        />
+        <Route 
+          path="/interview" 
+          element={
+            <SecureRoute>
+              <InterviewPage />
+            </SecureRoute>
+          } 
+        />
+        <Route 
+          path="/upload" 
+          element={
+            <SecureRoute>
+              <ResumeUploader />
+            </SecureRoute>
+          } 
+        />
+        <Route 
+          path="/feedback" 
+          element={
+            <SecureRoute>
+              <FeedbackPage />
+            </SecureRoute>
+          } 
+        />
       </Routes>
     </Router>
   );
